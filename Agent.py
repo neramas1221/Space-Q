90b9f7c6043949c29c1f4441c6bd7edb323f23a5
neramas1221 <connorwheeler1997@gmail.com>
2018-07-03 12:59:01 +0100

    added act method to get action from the network

--- Agent.py
+++ Agent.py
@@ -9,7 +9,7 @@ from keras.optimizers import Adam
 class Agent:
 
 
-    def __init__ (self, states, actions)
+    def __init__ (self, states, actions):
         self.states = states
         self.actions = actions
         self.memory = deque(maxlen = 1000)
@@ -20,11 +20,18 @@ class Agent:
         self.learningRate = 0.001
         self.model = self.makeModel()
 
-    def makeModel(self)
-
+    def makeModel(self):
         model = Sequential()
         model.add(Dense(24,input_dim = self.states,activation = 'relu'))
         model.add(Dense(24,activation = 'relu'))
         model.add(Dense(self.actions, activation = 'linear'))
         model.compile(loss = 'mse',optimizer = Adam(lr = self.learningRate))
         return model
+    
+    def act(self,state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.actions)
+        actionList = self.model.predict(state)
+        return np.argmax(actionList[0])
+    
+    
