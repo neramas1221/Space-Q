404d22dd4f0b32eae376e250311f0d9c7a0b1560
neramas1221 <connorwheeler1997@gmail.com>
2018-07-03 16:11:07 +0100

    Fixed things states in code as they would break when running states didnt work as they where not arrays when used for predictions

--- Agent.py
+++ Agent.py
@@ -5,6 +5,7 @@ from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.utils import plot_model
 
 class Agent:
 
@@ -22,31 +23,37 @@ class Agent:
 
     def makeModel(self):
         model = Sequential()
-        model.add(Dense(24,input_dim = self.states,activation = 'relu'))
+        model.add(Dense(24,input_shape = self.states.shape,activation = 'relu'))
         model.add(Dense(24,activation = 'relu'))
-        model.add(Dense(self.actions, activation = 'linear'))
-        model.compile(loss = 'mse',optimizer = Adam(lr = self.learningRate))
+        model.add(Dense(self.actions, activation = 'softmax'))
+        model.compile(loss = 'mean_squared_error',optimizer = Adam(lr = self.learningRate))
+        model.summary()
+        plot_model(model, to_file='model_plot.png', show_shapes=True, 
+                   show_layer_names=True)
+        #model.predict(np.zeros((1000,100800)))
         return model
     
     def act(self,state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.actions)
-        actionList = self.model.predict(state)
+        actionList = self.model.predict(np.array([state]))
         return np.argmax(actionList[0])
     
     def addToMemory(self,state,action,reward,nextState,done):
-        self.memory.append(state,action,reward,nextState,done)
+        self.memory.append((state,action,reward,nextState,done))
         
     def replay(self, batchSize):
         batch = random.sample(self.memory,batchSize)
         for state,action,reward,nextState,done in batch:
             target = reward
+
             if not done:
-                target = (reward + self.gamma * np.amax(self.model.
-                                predict(nextState)[0]))
-            result = self.model.predict(state)
+               target = (reward + self.gamma * np.amax(self.model.
+                                        predict(np.array([nextState]))))
+            
+            result = self.model.predict(np.array([state]))
             result[0][action] = target
-            self.model.fit(state,target,epochs=1,verbose=0)
+            self.model.fit(np.array([state]),result,epochs=1,verbose=0)
         if self.epsilon > self.epsilonMini:
             self.epsilon *= self.epsilonDecay
         
--- Main.py
+++ Main.py
@@ -1,8 +1,40 @@
 import random
 import gym
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
 from tensorflow.python.client import device_lib
+from Agent import Agent
 print(device_lib.list_local_devices())
+
+Episodes = 100
+Trials = 1000
+env = gym.make('SpaceInvaders-v0')
+
+states = env.observation_space.sample()# shape
+states = np.array(states).flatten()
+print states.shape
+actions = env.action_space.n
+agent = Agent(states,actions)
+done = False
+batchSize = 1
+
+for i in range (Episodes):
+    state = env.reset().flatten()
+    #state = np.reshape(state,[1, states])
+    for p in range (Trials):
+        #print state.shape
+        env.render()
+        action = agent.act(state)
+        nextState,reward,done,_   = env.step(action)
+        nextState = np.array(nextState).flatten()
+        reward = reward if not done else +10
+        
+        #nextState = np.reshape(nextState,[1,states])
+        agent.addToMemory(state,action,reward,nextState,done)
+        state = nextState
+        if done:
+            print("episode {}/{}, score : {}, e: {:.2}".format(i,
+                      Episodes,p,agent.epsilon))
+            break
+        if len(agent.memory) > batchSize:
+                agent.replay(batchSize)
+    
