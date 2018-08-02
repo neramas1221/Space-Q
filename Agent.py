cbe52250b10c8268527df4a7c17310b04b3f8bbc
neramas1221 <connorwheeler1997@gmail.com>
2018-08-02 13:50:05 +0100

    fixed inpit issue on network

--- Agent.py
+++ Agent.py
@@ -4,6 +4,8 @@ import numpy as np
 from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Conv2D
+from keras.layers import Flatten
 from keras.optimizers import Adam
 from keras.utils import plot_model
 
@@ -23,37 +25,41 @@ class Agent:
 
     def makeModel(self):
         model = Sequential()
-        model.add(Dense(24,input_shape = self.states.shape,activation = 'relu'))
-        model.add(Dense(24,activation = 'relu'))
-        model.add(Dense(self.actions, activation = 'softmax'))
+        model.add(Conv2D(64,3,3, border_mode='same',input_shape = (84,84,1),activation = 'relu'))
+        model.add(Flatten())
+        model.add(Dense(32,activation = 'relu'))
+        model.add(Dense(self.actions, activation = 'linear'))
         model.compile(loss = 'mean_squared_error',optimizer = Adam(lr = self.learningRate))
         model.summary()
-        plot_model(model, to_file='model_plot.png', show_shapes=True, 
+        plot_model(model, to_file='model_plot.png', show_shapes=True,
                    show_layer_names=True)
-        #model.predict(np.zeros((1000,100800)))
+        #print("model")
+        #model.predict(np.zeros((1,84,84,1)))
         return model
-    
+
     def act(self,state):
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.actions)
+        #if np.random.rand() <= self.epsilon:
+            #return random.randrange(self.actions)
         actionList = self.model.predict(np.array([state]))
+        #print ("action list")
+        #print (actionList.shape)
         return np.argmax(actionList[0])
-    
+
     def addToMemory(self,state,action,reward,nextState,done):
         self.memory.append((state,action,reward,nextState,done))
-        
+
     def replay(self, batchSize):
         batch = random.sample(self.memory,batchSize)
         for state,action,reward,nextState,done in batch:
             target = reward
-
+            #print ("Next State")
+            #print (nextState.shape)
             if not done:
                target = (reward + self.gamma * np.amax(self.model.
                                         predict(np.array([nextState]))))
-            
+
             result = self.model.predict(np.array([state]))
             result[0][action] = target
             self.model.fit(np.array([state]),result,epochs=1,verbose=0)
         if self.epsilon > self.epsilonMini:
             self.epsilon *= self.epsilonDecay
-        
--- Main.py
+++ Main.py
@@ -1,40 +1,55 @@
 import random
 import gym
 import numpy as np
+import cv2
 from tensorflow.python.client import device_lib
 from Agent import Agent
 print(device_lib.list_local_devices())
 
-Episodes = 100
-Trials = 1000
+def updateImage(state):
+    state = cv2.cvtColor(cv2.resize(state,(84,110)),cv2.COLOR_BGR2GRAY)
+    #remove the first 26 layers
+    state = state[26:110,:]
+    _,state = cv2.threshold(state, 1,255,cv2.THRESH_BINARY)
+    #print(state.shape)
+    state = np.reshape(state,(84,84,1))
+    return state#.reshape(1,state.shape[0],state.shape[1],state.shape[2])
+
+Episodes = 1000
+Trials = 500
 env = gym.make('SpaceInvaders-v0')
 
-states = env.observation_space.sample()# shape
-states = np.array(states).flatten()
-print states.shape
+states = env.observation_space.shape
+
 actions = env.action_space.n
+#print(actions)
 agent = Agent(states,actions)
 done = False
-batchSize = 1
+batchSize = 32
 
 for i in range (Episodes):
-    state = env.reset().flatten()
-    #state = np.reshape(state,[1, states])
-    for p in range (Trials):
-        #print state.shape
+    state = env.reset()
+    state = updateImage(state)
+    #print ("State shape")
+    #print (state.shape)
+
+
+    while not done:
+        #print (state.shape)
         env.render()
         action = agent.act(state)
-        nextState,reward,done,_   = env.step(action)
-        nextState = np.array(nextState).flatten()
-        reward = reward if not done else +10
-        
+        nextState,reward,done,inf   = env.step(action)
+        nextState = updateImage(nextState)
+        #print("next state")
+        #print(len(state))
+        #reward = reward if not done else 0.01
         #nextState = np.reshape(nextState,[1,states])
         agent.addToMemory(state,action,reward,nextState,done)
         state = nextState
         if done:
             print("episode {}/{}, score : {}, e: {:.2}".format(i,
-                      Episodes,p,agent.epsilon))
+                      Episodes,reward,agent.epsilon))
             break
         if len(agent.memory) > batchSize:
                 agent.replay(batchSize)
-    
+    done = False
